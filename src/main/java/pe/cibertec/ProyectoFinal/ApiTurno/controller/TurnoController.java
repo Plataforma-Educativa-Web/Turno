dd7f3f4ba73ab43209c802ba71ff59da8ed897bb
Victor Borja <vborja2424@gmail.com>
2023-11-26 17:00:50 -0500

    ApiTurno Terminado

--- src/main/java/pe/cibertec/ProyectoFinal/ApiTurno/controller/TurnoController.java
+++ src/main/java/pe/cibertec/ProyectoFinal/ApiTurno/controller/TurnoController.java
@@ -1,6 +1,7 @@
 package pe.cibertec.ProyectoFinal.ApiTurno.controller;
 
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,63 +14,107 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pe.cibertec.ProyectoFinal.ApiTurno.entity.Turno;
+import pe.cibertec.ProyectoFinal.ApiTurno.exception.EntityNotFoundException;
 import pe.cibertec.ProyectoFinal.ApiTurno.service.TurnoService;
 
 @RestController
 @RequestMapping("api/v1/turno")
+@Slf4j
 
 public class TurnoController {
-    
+
     @Autowired
-    
+
     private TurnoService turnoService;
-    
+
     @GetMapping("/findAll")
-    
+
     public ResponseEntity<List<Turno>> findAll() {
-        
-        return new ResponseEntity<>(turnoService.findAll(), HttpStatus.OK);
-        
+
+        try {
+            log.info("Endpoint: /api/v1/turno/findAll - Buscando todos los turnos");
+            List<Turno> turnos = turnoService.findAll();
+            return new ResponseEntity<>(turnos, HttpStatus.OK);
+        } catch (Exception e) {
+            log.error("Error en buscar todos: {}", e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
     }
-    
+
     @GetMapping("/findByNombre/{nombreH}")
-    
+
     public ResponseEntity<Turno> findByNombreH(@PathVariable String nombreH) {
-        
-        return new ResponseEntity<>(turnoService.findByNombreH(nombreH), HttpStatus.OK);
-        
+
+        try {
+            log.info("Endpoint: /api/v1/turno/findByNombre/{} - Buscando turno por nombre: {}", nombreH, nombreH);
+            Turno turno = turnoService.findByNombreH(nombreH);
+            return new ResponseEntity<>(turno, HttpStatus.OK);
+        } catch (EntityNotFoundException e) {
+            log.error("Error al buscar turno por nombre {}: {}", nombreH, e.getMessage());
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+        }
     }
-    
+
     @GetMapping("/buscarPorId/{id}")
-    
+
     public ResponseEntity<Turno> buscarPorId(@PathVariable Long id) {
-        
-        return new ResponseEntity<>(turnoService.buscarPorId(id), HttpStatus.OK);
-        
-    } 
-    
-    @PostMapping ("/agregarTurno")
-    
+
+        try {
+            log.info("Endpoint: /api/v1/turno/buscarPorId/{} - Buscando turno por ID: {}", id, id);
+            Turno turno = turnoService.buscarPorId(id);
+            return new ResponseEntity<>(turno, HttpStatus.OK);
+        } catch (EntityNotFoundException e) {
+            log.error("Error al buscar turno por ID : {}", e.getMessage());
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+        }
+    }
+
+    @PostMapping("/agregarTurno")
+
     public ResponseEntity<Turno> agregarTurno(@RequestBody Turno turno) {
-        
-        return new ResponseEntity<>(turnoService.agregarTurno(turno), HttpStatus.CREATED);
-        
+
+        try {
+            log.info("Endpoint: /api/v1/turno/agregarTurno - Agregando nuevo turno: {}", turno);
+            Turno nuevoTurno = turnoService.agregarTurno(turno);
+            return new ResponseEntity<>(nuevoTurno, HttpStatus.CREATED);
+        } catch (Exception e) {
+            log.error("Error en la operación: {}", e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
     }
-    
-    @PutMapping ("/actualizarTurno")
-    
+
+    @PutMapping("/actualizarTurno")
+
     public ResponseEntity<Turno> actualizarTurno(@RequestBody Turno turno) {
-        
-        return new ResponseEntity<>(turnoService.actualizarTurno(turno), HttpStatus.OK);
-        
+
+        try {
+            log.info("Endpoint: /api/v1/turno/actualizarTurno - Actualizando turno con ID {}: {}", turno.getId(), turno);
+            Turno turnoActualizado = turnoService.actualizarTurno(turno);
+            return new ResponseEntity<>(turnoActualizado, HttpStatus.OK);
+        } catch (EntityNotFoundException e) {
+            log.error("Error al actualizar turno con ID {}: {}", turno.getId(), e.getMessage());
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
-    
+
     @DeleteMapping("/eliminarTurno/{id}")
-    
-    public void eliminarTurno (@PathVariable Long id) {
-        
-        turnoService.eliminarTurno(id);
-        
+
+    public void eliminarTurno(@PathVariable Long id) {
+
+        try {
+
+            log.info("Endpoint: /api/v1/turno/eliminarTurno/{} - Eliminando turno con ID: {}", id, id);
+            turnoService.eliminarTurno(id);
+        } catch (Exception e) {
+
+            log.error("Error al eliminarTurno con el ID {} : {}", e.getMessage());
+
+        }
+
     }
-    
+
 }
--- src/main/java/pe/cibertec/ProyectoFinal/ApiTurno/serviceImpl/TurnoServiceImpl.java
+++ src/main/java/pe/cibertec/ProyectoFinal/ApiTurno/serviceImpl/TurnoServiceImpl.java
@@ -1,6 +1,7 @@
 package pe.cibertec.ProyectoFinal.ApiTurno.serviceImpl;
 
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pe.cibertec.ProyectoFinal.ApiTurno.dao.TurnoRepository;
@@ -9,59 +10,64 @@ import pe.cibertec.ProyectoFinal.ApiTurno.exception.EntityNotFoundException;
 import pe.cibertec.ProyectoFinal.ApiTurno.service.TurnoService;
 
 @Service
+@Slf4j
 
 public class TurnoServiceImpl implements TurnoService {
-    
+
     @Autowired
-    
+
     private TurnoRepository turnoRepository;
 
     @Override
     public List<Turno> findAll() {
-        
+
+        log.info("Buscando todos los turnos");
         return (List<Turno>) turnoRepository.findAll();
-        
+
     }
 
     @Override
     public Turno findByNombreH(String nombreH) {
-        
-        return turnoRepository.findByNombreH(nombreH).orElseThrow(() -> new EntityNotFoundException("Turno no encontrada con el nombre"+nombreH.toString()));
-        
+
+        log.info("Buscando turno por nombre: {}", nombreH);
+        return turnoRepository.findByNombreH(nombreH).orElseThrow(() -> new EntityNotFoundException("Turno no encontrado con el nombre " + nombreH));
+
     }
 
     @Override
     public Turno buscarPorId(Long id) {
-        
-        return turnoRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Turno no encontrado por el id"+id.toString()));
+
+        log.info("Buscando turno por ID: {}", id);
+        return turnoRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Turno no encontrado por el ID " + id));
+
     }
 
     @Override
     public Turno agregarTurno(Turno turno) {
-        
+
+        log.info("Agregando nuevo turno: {}", turno);
         return turnoRepository.save(turno);
-        
+
     }
 
     @Override
     public Turno actualizarTurno(Turno turno) {
-        
-        var TurnoF = turnoRepository.findById(turno.getId()).get();
-        TurnoF.setNombreH(turno.getNombreH());
-        TurnoF.setDescripcion(turno.getDescripcion());
-        
-        return turnoRepository.save(TurnoF);
-         
+
+        log.info("Actualizando turno con ID {}: {}", turno.getId(), turno);
+        var turnoEncontrado = turnoRepository.findById(turno.getId()).orElseThrow(() -> new EntityNotFoundException("Turno no encontrado por el ID " + turno.getId()));
+        turnoEncontrado.setNombreH(turno.getNombreH());
+        turnoEncontrado.setDescripcion(turno.getDescripcion());
+        return turnoRepository.save(turnoEncontrado);
+
     }
 
     @Override
     public void eliminarTurno(Long id) {
-        
-        var TurnoF = turnoRepository.findById(id).get();
-        
-        turnoRepository.delete(TurnoF);
-        
-        
+
+        log.info("Eliminando turno con ID: {}", id);
+        var turnoEncontrado = turnoRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Turno no encontrado por el ID " + id));
+        turnoRepository.delete(turnoEncontrado);
+
     }
-    
+
 }
